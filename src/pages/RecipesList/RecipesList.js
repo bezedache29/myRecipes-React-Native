0ce9be63d6596fe949e3186d0295922c576aba7c
Christophe Salou <bezedache29@gmail.com>
2021-10-02 16:14:33 +0200

    Init du projet, nettoyage du fichier App.js

---
+++ src/pages/RecipesList/RecipesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const RecipesList = () => {
+  return (
+    <>
+      <Text>Recipes List</Text>
+    </>
+  )
+}
+
+export default RecipesList
