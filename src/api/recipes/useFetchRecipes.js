8296ef6a74d7913f23accdc85247959a4e66dc5f
Christophe Salou <bezedache29@gmail.com>
2021-10-02 18:26:29 +0200

    Requete axios avec le hook perso

--- src/api/recipes/useFetchRecipes.js
+++ src/api/recipes/useFetchRecipes.js
@@ -1,21 +1,23 @@
-import { axios } from "axios"
-import {API_URL, API_KEY} from "@env"
+import axios from "axios"
 
 // Custom Hooks pour récupérer toute les recettes
 // const { getAllRecipes } = useFetchRecipes()
 
 // Les customs Hooks sont des fonctions qui retournent un certains nombre de propriétés
 
-
-const MAX_RESULTS = 30
+const API_URL = "https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY = "5f573a11810f46c6b85edf648cd715ba"
+const MAX_PER_PAGE = 30
 
 export const useFetchRecipes = () => {
 
   const getAllRecipes = async () => {
     try {
       const response = await axios.get(API_URL, {
-        apiKey: API_KEY,
-        number: MAX_RESULTS
+        params: {
+          apiKey: API_KEY,
+          number: MAX_PER_PAGE
+        }
       })
       console.log('response', response.data.results)
 
--- src/pages/RecipesList/RecipesList.js
+++ src/pages/RecipesList/RecipesList.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
+import { useFetchRecipes } from '../../api/recipes/useFetchRecipes'
 
 const RecipesList = () => {
+
+  const { getAllRecipes } = useFetchRecipes()
+
+  useEffect(() => {
+    getAllRecipes()
+  }, [])
+
   return (
     <>
       <Text>Recipes List</Text>
